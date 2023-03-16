778da38b0383931ca1c2580af168ecbc355bfd5c
glennancy <glennancy24@gmail.com>
2023-03-15 21:23:40 -0300

    first commit

---
+++ DiceRoller/app/src/main/java/com/ds2/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ds2.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.btnRollDice)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "ROlou o dado!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resulTextView: TextView = findViewById(R.id.textView)
+        resulTextView.text = diceRoll.toString()
+    }
+}
+class Dice (private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
